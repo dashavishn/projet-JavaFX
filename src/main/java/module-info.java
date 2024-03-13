768a81baa4926589737183c3d9db11bf682c1a94
dashavishn <daryavishnyak@gmail.com>
2024-03-13 22:13:43 +0100

    test

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module bigbrain.java_bureau {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens bigbrain.java_bureau to javafx.fxml;
+    exports bigbrain.java_bureau;
+}
