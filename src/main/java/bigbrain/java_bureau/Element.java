af5a6a4c26f2d2ff9c94f43dfc2c032b582236e4
hanja19 <hanjarj19@gmail.com>
2024-03-15 21:43:04 +0100

    classe elements hanja

--- src/main/java/bigbrain/java_bureau/Element.java
+++ src/main/java/bigbrain/java_bureau/Element.java
@@ -1,4 +1,29 @@
 package bigbrain.java_bureau;
 
 public class Element {
+    private String code;
+    private String nom;
+    private double quantiteStock;
+    private String uniteMesure;
+
+    public void Element(String code, String nom, double quantiteStock, String uniteMesure) {
+        this.code=code;
+        this.nom=nom;
+        this.quantiteStock=quantiteStock;
+        this.uniteMesure=uniteMesure;
+    }
+
+    }
+
+
+
+
+
+
+
+
+
+
 }
+
+
