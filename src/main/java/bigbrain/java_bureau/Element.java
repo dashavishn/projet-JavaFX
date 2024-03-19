1aef27ea3599d4f277da47aeb90b05cc1bf0321d
hanja19 <hanjarj19@gmail.com>
2024-03-19 11:29:31 +0100

    Classe element HAnja

--- src/main/java/bigbrain/java_bureau/Element.java
+++ src/main/java/bigbrain/java_bureau/Element.java
@@ -6,20 +6,51 @@ public class Element {
     private double quantiteStock;
     private String uniteMesure;
 
+    private double prixAchat;
 
-    public Element(String code, String nom, double quantiteStock, String uniteMesure) {
+    private double prixVente;
+
+
+    public Element(String code, String nom, double quantiteStock, String uniteMesure, double prixAchat, double prixVente) {
         this.code=code;
         this.nom=nom;
         this.quantiteStock=quantiteStock;
         this.uniteMesure=uniteMesure;
+        this.prixAchat=prixAchat;
+        this.prixVente=prixVente;
     }
 
-    public void ajouetrQuanite(double quantite){
+    public void ajouterQuantite(double quantite){
         this.quantiteStock += quantite;
     }
     public void retirerQuantite (double quantite){
         this.quantiteStock -= quantite;
 
+    }
+
+    public double getQuantiteStock(){
+        return quantiteStock;
+
+    }
+    public String getCode(){
+        return code;
+    }
+
+
+    public String getNom(){
+        return nom;
+    }
+
+    public String getUniteMesure() {
+        return uniteMesure;
+    }
+
+    public double getPrixAchat(){
+        return prixAchat;
+    }
+
+    public double getPrixVente() {
+        return prixVente;
     }
 
     public boolean estDisponible(double quantite){
