cc250ae0dc076eeed30502017442a4f80a4ca6d1
hanja19 <hanjarj19@gmail.com>
2024-04-22 23:46:03 +0200

    controller Chaine

--- src/main/java/bigbrain/java_bureau/Chaine.java
+++ src/main/java/bigbrain/java_bureau/Chaine.java
@@ -1,4 +1,19 @@
 package bigbrain.java_bureau;
 
-public class Chaine {
+import javafx.fxml.Initializable;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.event.ActionEvent;
+public class Chaine implements Initializable {
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        // Initialisation du contrôleur
+    }
+
+    @FXML
+    private void validerBoutton(ActionEvent event) {
+        System.out.println("Le bouton VALIDER a été cliqué !");
+    }
 }
