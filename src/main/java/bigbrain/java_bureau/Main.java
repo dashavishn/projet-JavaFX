b743ce06949a70eb78e0b2c501e08752ca276d66
hanja19 <hanjarj19@gmail.com>
2024-04-25 19:40:32 +0200

    COMMIT MAIN

--- src/main/java/bigbrain/java_bureau/Main.java
+++ src/main/java/bigbrain/java_bureau/Main.java
@@ -2,23 +2,48 @@ package bigbrain.java_bureau;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class Main extends Application {
+
+    protected static Stage primaryStage;
+
     @Override
-    public void start(Stage stage) throws IOException {
+    public void start(Stage primaryStage) throws IOException {
+        Main.primaryStage = primaryStage;
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("stock.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
+        primaryStage.setTitle("Hello!");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public void ChargerPage(String page) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource(page));
+        Parent root = null;
+        try {
+            root = loader.load();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+    public static Stage getPrimaryStage(){
+        return primaryStage;
     }
 
     public static void main(String[] args) {
-        System.out.println(".");
+        CSV b = new CSV();
+        b.LireElement();
+        b.LireHistorique();
+        b.LireChaine();
+        Stocks.copieStock();
         launch();
     }
 }
