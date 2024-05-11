4729b1029d3035f578a07ce71c2d8ab76f6723cb
hanja19 <hanjarj19@gmail.com>
2024-05-11 20:10:18 +0200

    Big Commit hanja

--- src/main/java/bigbrain/java_bureau/controller/Accueil_Controller.java
+++ src/main/java/bigbrain/java_bureau/controller/Accueil_Controller.java
@@ -1,36 +1,64 @@
 package bigbrain.java_bureau.controller;
-import bigbrain.java_bureau.classe_java.*;
 
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.*;
-import javafx.scene.control.cell.PropertyValueFactory;
-import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.AnchorPane;
+import javafx.scene.control.Button;
 import javafx.stage.Stage;
-import org.w3c.dom.Text;
 
 import java.io.IOException;
 import java.net.URL;
 import java.util.Objects;
 import java.util.ResourceBundle;
 
-public class Accueil_Controller implements Initializable{
+public class Accueil_Controller implements Initializable {
+
     @FXML
     private Button stock;
+    @FXML
     private Button chaine;
+    @FXML
     private Button commandes;
+    @FXML
     private Button historique;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        // Initialisation spécifique, si nécessaire
     }
 
-}
+    @FXML
+    private void Page_Stock() {
+        ChargerPage("/bigbrain/java_bureau/stock.fxml");
+    }
 
+    @FXML
+    private void Page_Chaine() {
+        ChargerPage("/bigbrain/java_bureau/chaine.fxml");
+    }
+
+    @FXML
+    private void Page_Commandes() {
+        ChargerPage("/bigbrain/java_bureau/commandes.fxml");
+    }
+
+    @FXML
+    private void Page_Historique() {
+        ChargerPage("/bigbrain/java_bureau/historique.fxml");
+    }
+
+    private void ChargerPage(String page) {
+        try {
+            FXMLLoader loader = new FXMLLoader(Objects.requireNonNull(getClass().getResource(page)));
+            Parent root = loader.load();
+            Scene scene = new Scene(root);
+            Stage stage = (Stage) stock.getScene().getWindow();
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
