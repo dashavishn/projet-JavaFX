d23b48e1773e29c28e4aaacad0a28e97c1862436
dashavishn <daryavishnyak@gmail.com>
2024-05-12 19:35:25 +0200

    Dasha: chaine.fxml modif

--- src/main/java/bigbrain/java_bureau/controller/ChaineController.java
+++ src/main/java/bigbrain/java_bureau/controller/ChaineController.java
@@ -4,6 +4,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
