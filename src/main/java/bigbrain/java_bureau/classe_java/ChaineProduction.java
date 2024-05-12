e12fcf6b3dab239424be41e58b9feaf56584719a
hanja19 <hanjarj19@gmail.com>
2024-05-12 19:42:00 +0200

    HAnja : fxml chaine de prod

--- src/main/java/bigbrain/java_bureau/classe_java/ChaineProduction.java
+++ src/main/java/bigbrain/java_bureau/classe_java/ChaineProduction.java
@@ -1,7 +1,9 @@
 package bigbrain.java_bureau.classe_java;
 
+
 import java.util.HashMap;
 import java.util.HashSet;
+
 
 public class ChaineProduction {
     private String code;
@@ -9,6 +11,7 @@ public class ChaineProduction {
     public int niveauActivation;
     HashMap<Element, Float> elementEntree = new HashMap<>();
     HashMap<Element, Float> elementSortie = new HashMap<>();
+
 
     public ChaineProduction(String code, String nom, HashMap<Element, Float> elementEntree, HashMap<Element, Float> elementSortie) {
         this.code = code;
@@ -18,59 +21,70 @@ public class ChaineProduction {
         this.elementSortie = elementSortie;
     }
 
+
     public void ajouterElementEntree(Element element, float quantite) {
         elementEntree.put(element, quantite);
     }
+
 
     public void ajouterElementSortie(Element element, float quantite) {
         elementSortie.put(element, quantite);
     }
 
+
     public void removeElemEntree(Element e) {
         elementEntree.remove(e);
     }
+
 
     public void removeElemSortie(Element e) {
         elementSortie.remove(e);
     }
 
+
     public String getCode() {
         return this.code;
     }
+
 
     public String getNom() {
         return this.nom;
     }
 
+
     public int getNiveauActivation() {
         return this.niveauActivation;
     }
+
 
     public void setNiveauActivation(int niveauA) {
         this.niveauActivation = niveauA;
     }
 
-   /* public HashMap<Element, Float> simulerProduction() {
-        HashMap<Element, Float> productionResultat = new HashMap<>();
-        for (Element e : elementEntree.keySet()) {
-            float quantiteNecessaire = elementEntree.get(e) * this.niveauActivation;
-            if (Stocks.verifierDisponibilite(e, quantiteNecessaire)) {
-                Stocks.retirerStock(e, quantiteNecessaire);
-            } else {
-                System.out.println("Stock insuffisant pour " + e.getNom());
-                return null; // Arrête la production si le stock est insuffisant
-            }
-        }
 
-        for (Element e : elementSortie.keySet()) {
-            float quantiteProduite = elementSortie.get(e) * this.niveauActivation;
-            productionResultat.put(e, quantiteProduite);
-            Stocks.ajouterStock(e, quantiteProduite);
-        }
-        return productionResultat;
-    }
+    /* public HashMap<Element, Float> simulerProduction() {
+         HashMap<Element, Float> productionResultat = new HashMap<>();
+         for (Element e : elementEntree.keySet()) {
+             float quantiteNecessaire = elementEntree.get(e) * this.niveauActivation;
+             if (Stocks.verifierDisponibilite(e, quantiteNecessaire)) {
+                 Stocks.retirerStock(e, quantiteNecessaire);
+             } else {
+                 System.out.println("Stock insuffisant pour " + e.getNom());
+                 return null; // Arrête la production si le stock est insuffisant
+             }
+         }
 
-    */
+
+         for (Element e : elementSortie.keySet()) {
+             float quantiteProduite = elementSortie.get(e) * this.niveauActivation;
+             productionResultat.put(e, quantiteProduite);
+             Stocks.ajouterStock(e, quantiteProduite);
+         }
+         return productionResultat;
+     }
+
+
+     */
     public void simulerEtAfficherResultats() {
         HashMap<Element, Float> resultats = simulerProduction();
         if (resultats == null) {
@@ -82,9 +96,11 @@ public class ChaineProduction {
             );
         }
     }
+
     public HashMap<Element, Float> simulerProduction() {
         HashMap<Element, Float> productionResultat = new HashMap<>();
         boolean stockSuffisant = true;
+
 
         // Vérifie d'abord la disponibilité de tous les éléments nécessaires
         for (Element e : elementEntree.keySet()) {
@@ -96,12 +112,14 @@ public class ChaineProduction {
             }
         }
 
+
         if (stockSuffisant) {
             // Consomme les éléments d'entrée
             for (Element e : elementEntree.keySet()) {
                 float quantiteNecessaire = elementEntree.get(e) * this.niveauActivation;
                 Stocks.retirerStock(e, quantiteNecessaire);
             }
+
 
             // Produit et ajoute les éléments de sortie au stock
             for (Element e : elementSortie.keySet()) {
@@ -113,10 +131,9 @@ public class ChaineProduction {
             return null;  // Retourne null si le stock était insuffisant pour une ou plusieurs entrées
         }
 
+
         return productionResultat;
     }
-
-
 
 
 }
